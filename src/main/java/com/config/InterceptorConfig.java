e2a7b3a59cf08c44d77e58024c220af1f893404e
Chiwah <986771093@qq.com>
2025-05-04 23:07:09 +0800

    fix: 鉴权忽略 upload 路径 的文件

--- src/main/java/com/config/InterceptorConfig.java
+++ src/main/java/com/config/InterceptorConfig.java
@@ -27,6 +27,13 @@ public class InterceptorConfig extends WebMvcConfigurationSupport{
 	 */
 	@Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // 映射 /upload/** 到本地文件系统或 classpath 下对应目录
+        registry.addResourceHandler("/upload/**")
+            // 如果你是把文件放在项目根目录的 upload 里，就用 file:
+            // .addResourceLocations("file:upload/")
+            // 如果是 classpath 下的 upload 目录，则：
+            .addResourceLocations("classpath:/upload/");
+            
 		registry.addResourceHandler("/**")
         .addResourceLocations("classpath:/resources/")
         .addResourceLocations("classpath:/static/")
